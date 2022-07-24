88bd5997f15db64d8d09a24b35d5ca27c88e6578
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-07-24 13:45:52 +0200

    Base Mod Files

---
+++ Main.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using BepInEx;
+using Corale.Colore.Core;
+using HarmonyLib;
+
+namespace IsometricRGB
+{
+    [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string MOD_GUID = "io.raoul1808.isometricrgb";
+        public const string MOD_NAME = "Isometric RGB";
+        public const string MOD_VERSION = "1.0.0";
+        
+        void Awake()
+        {
+            Chroma.Instance.Initialize();
+            Thread.Sleep(1000);
+            Chroma.Instance.SetAll(new Color(0, 0, 0));
+            Harmony.CreateAndPatchAll(typeof(RgbPatches));
+        }
+
+        class RgbPatches
+        {
+            [HarmonyPatch(typeof(PaletteBehaviour), nameof(PaletteBehaviour.OnBeat))]
+            [HarmonyPostfix]
+            private static void UpdateKeyboardRgb()
+            {
+                var c = PaletteBehaviour.Instance.CurrentBackgroundColor;
+                Chroma.Instance.SetAll(new Color(c.r, c.g, c.b));
+            }
+        }
+    }
+}
